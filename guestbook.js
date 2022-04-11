08b9cd1395f6cca7074065f64d92b03830e9124c
h3X <turlacu_r@yahoo.com>
2022-04-11 22:21:22 +0300

    Update API URL
    View the guestbook.js file. Notice that the apiUrl constant is blank. In the quotes, put the route to your API, which you should have noted in the last step.

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0e97e6bd.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
